c5f27f8a571468f911bd68ede569fac09c4c9604
Refren86 <denis1975shvaiber@gmail.com>
2023-06-02 21:39:45 +0300

    gogogo

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["upload.wikimedia.org", "res.cloudinary.com"],
+    domains: ['upload.wikimedia.org', 'res.cloudinary.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ivantique.vercel.app/:path*',
+      },
+    ];
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
